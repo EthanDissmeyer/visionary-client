5d6afd934a13a4e4e894a708805b4d6fc1ebd8d9
root <root@srv701277.hstgr.cloud>
2025-01-25 03:42:23 +0000

    Changes made on the VPS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // Enable trailing slash handling if necessary
+    trailingSlash: false,
+
+    // Enable output files for server rendering
+    output: 'standalone',
+
+    // Uncomment if hosting assets on a specific domain or CDN
+    // assetPrefix: 'https://visionary.host',
+};
 
 export default nextConfig;
+
